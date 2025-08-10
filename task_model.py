782085b00bab1ebdcba96ad71dad7f8dde2cc784
Jhon-E <jhoneospino@gmail.com>
2025-08-09 19:44:31 -0500

    Resolver conflicto combinando metodos y atributos

--- task_model.py
+++ task_model.py
@@ -2,16 +2,16 @@ class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
         self.is_completed = False
-        self.is_done = False
 
     def get_task_name(self):
         return self.task_name
     
     def mark_as_complete(self):
-        self.is_comnplete = True
+        self.is_completed = True
     
     def is_completed(self):
         return self.is_completed
+    
     def set_done(self):
         self.is_done = True
 
